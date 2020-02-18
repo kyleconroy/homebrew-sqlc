2c1adc93fcaa8e99e5f94761bb440ed96edd1887
Kyle Conroy <kyle@conroy.org>
2020-02-18 11:15:42 -0800

    Release v1.0.0

--- Formula/sqlc.rb
+++ Formula/sqlc.rb
@@ -1,17 +1,11 @@
 class Sqlc < Formula
   desc "Generate type safe Go from SQL"
   homepage "https://sqlc.dev/"
-  url "https://github.com/kyleconroy/sqlc/archive/v0.1.0.tar.gz"
-  sha256 "5c693e24579e42478d08ed693785054cf97d8ae362b324e923029f54b1313fa5"
+  url "https://github.com/kyleconroy/sqlc/archive/v1.0.0.tar.gz"
+  sha256 "40443ef8b63f57d731d4cff21134bc6a07ab949e3f3a489a5cffd4c5808e3932"
   head "https://github.com/kyleconroy/sqlc.git"
 
   depends_on "go" => :build
-
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-sqlc"
-    cellar :any_skip_relocation
-    sha256 "57f8ce52e3872630ae62c2a065afd900b211c5a35cd8a2fcc9c13a5b0d2d8bf0" => :mojave
-  end
 
   def install
     ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
