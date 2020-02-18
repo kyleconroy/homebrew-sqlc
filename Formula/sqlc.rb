219e2ded70af6293f0604b3066ffd46944a7116b
Kyle Conroy <kyle@conroy.org>
2020-02-18 15:15:47 -0800

    Return the correct version number

--- Formula/sqlc.rb
+++ Formula/sqlc.rb
@@ -1,8 +1,9 @@
 class Sqlc < Formula
   desc "Generate type safe Go from SQL"
   homepage "https://sqlc.dev/"
-  url "https://github.com/kyleconroy/sqlc/archive/v1.0.0.tar.gz"
-  sha256 "40443ef8b63f57d731d4cff21134bc6a07ab949e3f3a489a5cffd4c5808e3932"
+  # TODO: Use a tagged release for v1.1.0
+  url "https://github.com/kyleconroy/sqlc/tarball/d8c0915ca5a51d715dba604cb3a32026b12283d1"
+  sha256 "0ccddf2a9a56b78d4f0ebfcfb99fd1ccb80a4e45fc5ba8115e4a3b603d7ea6d6"
   head "https://github.com/kyleconroy/sqlc.git"
 
   depends_on "go" => :build
@@ -12,7 +13,7 @@ class Sqlc < Formula
     (buildpath/"src/github.com/kyleconroy/sqlc").install buildpath.children
 
     cd "src/github.com/kyleconroy/sqlc" do
-      system "go", "build", "-o", bin/"sqlc", "--ldflags=\"-X 'github.com/kyleconroy/sqlc/internal/cmd.version=v1.0.0'\"", "./cmd/sqlc"
+      system "go", "build", "-o", bin/"sqlc", "./cmd/sqlc"
     end
   end
 end
