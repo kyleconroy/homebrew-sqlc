7025ca87a62cc8c40e084fa4b282cea77e4ac06c
Kyle Conroy <kyle@conroy.org>
2020-08-05 12:21:56 -0700

    Publish sqlc v1.5.0

--- Formula/sqlc.rb
+++ Formula/sqlc.rb
@@ -1,8 +1,8 @@
 class Sqlc < Formula
   desc "Generate type safe Go from SQL"
   homepage "https://sqlc.dev/"
-  url "https://github.com/kyleconroy/sqlc/archive/v1.4.0.tar.gz"
-  sha256 "77a09f42cc88e59e6788579628c8606760851f72488133595522825695baf892"
+  url "https://github.com/kyleconroy/sqlc/archive/v1.5.0.tar.gz"
+  sha256 "9000a71974c0327e6fda5f46421bb6d58b102164046d271c6f174dd5f437e99e"
   head "https://github.com/kyleconroy/sqlc.git"
 
   depends_on "go" => :build
